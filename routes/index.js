ce7458f2262c1aff2105237320573fa8bf9ca024
Farhan-Haseeb <syedfarhanhaseeb@gmail.com>
2019-10-25 18:27:37 +0500

    Rendering members

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,43 @@
-export default function routes(app, addon) {
-    // Redirect root path to /atlassian-connect.json,
-    // which will be served by atlassian-connect-express.
-    app.get('/', (req, res) => {
+export default function routes(app, addon, jira) {
+    
+   app.get('/', (req, res) => {
         res.redirect('/atlassian-connect.json');
     });
 
-    // This is an example route used by "generalPages" module (see atlassian-connect.json).
-    // Verify that the incoming request is authenticated with Atlassian Connect.
-    app.get('/hello-world', addon.authenticate(), (req, res) => {
-        // Rendering a template is easy; the render method takes two params:
-        // name of template and a json object to pass the context in.
-        res.render('hello-world', {
-            title: 'Atlassian Connect',            
-            req: req
-        });
-    });
+    app.get('/dashboard', addon.authenticate(), async (req, res) => {
+        // jira.project
+        //   .getAllProjects({
+        //     expand: "description,lead,issueTypes,url,projectKeys,permissions"
+        //   })
+        //   .then(data => {
+        //     console.log(data, "project data");
+        //   })
+        //   .catch(err => console.log(err, "that is an err"));
 
-    // Add additional route handlers here...
+        // jira.user.getUser({username: "syed.farhan"})
+        //     .then((data) => {
+        //         res.render('dashboard', {
+        //             title: 'Dashboard',     
+        //             data: data
+        //         });
+        //     })
+        //     .catch(err => console.log(err, 'err msg'))
+        var members = []
+        jira.user.all({maxResults: 50})
+        .then(data => {
+            data.forEach(member => {
+                if(member.accountType === 'atlassian'){
+                    members.push(member)
+                }
+            })
+            res.render('dashboard', {
+                title: 'Members',     
+                data: members
+            });
+        })
+        .catch(err => {
+            console.log(err, 'err is here')
+        })
+       
+    });
 }
