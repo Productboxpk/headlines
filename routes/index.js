5fbb8c3d5bc95ee2dd6d1979e13d10b406742f42
Farhan-Haseeb <syedfarhanhaseeb@gmail.com>
2019-10-31 17:16:21 +0500

    Some code refactor

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ export default function routes(app, addon, jira) {
 
   app.get("/headlines", addon.authenticate(), async (req, res) => {
     const projectKeys = [];
-    let issues = [];
+    let userIssues = [];
 
     await jira.project
       .getProject()
@@ -36,49 +36,48 @@ export default function routes(app, addon, jira) {
           expand: "changelog"
         })
         .then(data => {
-          data.issues.forEach(issue => {
-            issues.push({
-              key: issue.key,
-              fields: issue.fields,
-              histories: issue.changelog.histories[0]
+          const { issues } = data;
+          for (let i = 0; i <= issues.length - 1; i++) {
+            let items = _.filter(
+              issues[i].changelog.histories[0].items,
+              item => item.field === "assignee"
+            );
+
+            userIssues.push({
+              key: issues[i].key,
+              fields: issues[i].fields,
+              histories: items
             });
-          });
+          }
         })
         .catch(err => {
           console.log(err, "issues err is here");
         });
     }
 
-    for (let i = 0; i <= issues.length - 1; i++) {
-      issues[i].histories.items.avatarUrls;
-      const items = _.filter(
-        issues[i].histories.items,
-        item => item.field === "assignee"
-      );
-      issues[i].histories = items;
-      if (items.length && items[0].from) {
-        const from = items.length && items[0].from;
-
+    for (let i = 0; i <= userIssues.length - 1; i++) {
+      if (userIssues[i].histories.length && userIssues[i].histories[0].from) {
+        const from =
+          userIssues[i].histories.length && userIssues[i].histories[0].from;
         await jira.user
           .getUser({ userKey: from })
           .then(data => {
-            const avatars = data.avatarUrls;
-            issues[i].histories[0].avatars = avatars;
+            userIssues[i].histories[0].avatars = data.avatarUrls;
           })
           .catch(err => {
-            console.log(err, "issues err is here");
+            console.log(err, "get user err is here");
           });
       }
     }
 
-    issues = _.sortBy(issues, i => {
+    userIssues = _.sortBy(userIssues, i => {
       return i.fields.updated;
     });
-    issues = _.reverse(issues);
+    userIssues = _.reverse(userIssues);
 
     res.render("headlines", {
       title: "Issues",
-      data: issues
+      data: userIssues
     });
   });
 }
