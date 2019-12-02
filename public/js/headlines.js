5d6f6063415cf6d204aabcab3ff1e4ea4d30df2c
Farhan-Haseeb <syedfarhanhaseeb@gmail.com>
2019-12-02 13:59:45 +0500

    Handled repo filters in js side

--- public/js/headlines.js
+++ public/js/headlines.js
@@ -1,97 +1,99 @@
 document.onreadystatechange = function() {
-  if (document.readyState == "interactive") {
-    let jwt_token = $('meta[name="token"]').attr("content");
-    let projectKeys = [];
+    if (document.readyState == "interactive") {
+        let jwt_token = $('meta[name="token"]').attr("content");
+		let projectKeys = [];
+		let repoNames = [];
 
-    // Shows the dialog when the "Show dialog" button is clicked
-    AJS.$("#dialog-show-button").click(function(e) {
-      e.preventDefault();
-      AJS.dialog2("#credential-dialog").show();
-    });
+        function addEventListeners() {
+			let jiraProjects = document.getElementsByClassName("project-link");
+			let githubRepos = document.getElementsByClassName("repositories-link");
+            let loader = document.getElementsByClassName("loader")[0];
 
-    // Hides the dialog
-    AJS.$("#dialog-close-button").click(function(e) {
-      e.preventDefault();
-      AJS.dialog2("#credential-dialog").hide();
-    });
+			// asd--
 
-    function addEventListeners() {
-      let elements = document.getElementsByClassName("project-link");
-      let loader = document.getElementsByClassName("loader")[0];
+            for (let i = 0; i <= jiraProjects.length - 1; i++) {
+                const current = jiraProjects[i];
+                current.addEventListener("change", function(e) {
+                    loader.classList.remove("hide-loader");
+                    if (
+                        e.target.checked &&
+                        projectKeys.findIndex(key => key == e.target.name) === -1
+                    ) {
+                        projectKeys.push(e.target.name);
+                        sendSelectedProjects(projectKeys.toString());
+                    } else {
+                        const index = projectKeys.findIndex(key => key == e.target.name);
+                        projectKeys.splice(index, 1);
+                        sendSelectedProjects(projectKeys.toString());
+                    }
+                });
+			}
 
-      let username = document.getElementById("github-username");
-      let accessToken = document.getElementById("access-token");
-      let repoOwner = document.getElementById("repo-owner");
-      let repoName = document.getElementById("repo-name");
-      let saveButton = document.getElementById("save-button");
-
-      saveButton.addEventListener("onsubmit", (e) => {
-        e.preventDefault();
-
-        console.log(username.value);
-        console.log(accessToken.value);
-        console.log(repoOwner.value);
-        console.log(repoName.value);
-      });
-
-      username.addEventListener("keydown", data => {
-        data.key;
-      });
-
-      accessToken.addEventListener("keydown", data => {
-        data.key;
-      });
-
-      repoOwner.addEventListener("keydown", data => {
-        data.key;
-      });
-
-      repoName.addEventListener("keydown", data => {
-        data.key;
-      });
-
-      for (let i = 0; i <= elements.length - 1; i++) {
-        const current = elements[i];
-        current.addEventListener("change", function(e) {
-          loader.classList.remove("hide-loader");
-          if (
-            e.target.checked &&
-            projectKeys.findIndex(key => key == e.target.name) === -1
-          ) {
-            projectKeys.push(e.target.name);
-            sendReq(projectKeys.toString());
-          } else {
-            const index = projectKeys.findIndex(key => key == e.target.name);
-            projectKeys.splice(index, 1);
-            sendReq(projectKeys.toString());
-          }
-        });
-      }
-    }
-
-    function sendReq(projectkey) {
-      if (projectKeys.length > 0) {
-        let loader = document.getElementsByClassName("loader")[0];
-        var XHR = new XMLHttpRequest();
-
-        XHR.onreadystatechange = function() {
-          if (XHR.readyState == 4) {
-            if (XHR.status == 200) {
-              $("body").html(XHR.responseText);
-              loader.classList.add("hide-loader");
-              addEventListeners();
-              projectKeys = [];
-            } else {
-              console.log("There was a problem!!!");
+			for (let i = 0; i <= githubRepos.length - 1; i++) {
+                const current = githubRepos[i];
+                current.addEventListener("change", function(e) {
+                    loader.classList.remove("hide-loader");
+                    if (
+                        e.target.checked &&
+                        repoNames.findIndex(key => key == e.target.name) === -1
+                    ) {
+                        repoNames.push(e.target.name);
+                        sendSelectedRepositories(repoNames.toString());
+                    } else {
+                        const index = repoNames.findIndex(key => key == e.target.name);
+                        repoNames.splice(index, 1);
+                        sendSelectedRepositories(repoNames.toString());
+                    }
+                });
             }
-          }
-        };
+        }
 
-        XHR.open("GET", `headlines?projectKey=${projectkey}`);
-        XHR.setRequestHeader("Authorization", `JWT ${jwt_token}`);
-        XHR.send();
-      }
+        function sendSelectedProjects(projectkey) {
+            if (projectKeys.length > 0) {
+                let loader = document.getElementsByClassName("loader")[0];
+                var XHR = new XMLHttpRequest();
+
+                XHR.onreadystatechange = function() {
+                    if (XHR.readyState == 4) {
+                        if (XHR.status == 200) {
+                            $("body").html(XHR.responseText);
+                            loader.classList.add("hide-loader");
+                            addEventListeners();
+                            projectKeys = [];
+                        } else {
+                            console.log("There was a problem!!!");
+                        }
+                    }
+                };
+
+                XHR.open("GET", `headlines?projectKey=${projectkey}`);
+                XHR.setRequestHeader("Authorization", `JWT ${jwt_token}`);
+                XHR.send();
+            }
+		}
+		function sendSelectedRepositories(projectkey) {
+            if (repoNames.length > 0) {
+                let loader = document.getElementsByClassName("loader")[0];
+                var XHR = new XMLHttpRequest();
+
+                XHR.onreadystatechange = function() {
+                    if (XHR.readyState == 4) {
+                        if (XHR.status == 200) {
+                            $("body").html(XHR.responseText);
+                            loader.classList.add("hide-loader");
+                            addEventListeners();
+                            repoNames = [];
+                        } else {
+                            console.log("There was a problem!!!");
+                        }
+                    }
+                };
+
+                XHR.open("GET", `headlines?repoNames=${repoNames}`);
+                XHR.setRequestHeader("Authorization", `JWT ${jwt_token}`);
+                XHR.send();
+            }
+        }
+        addEventListeners();
     }
-    addEventListeners();
-  }
 };
