0256e6a1bf70cdfb2bf68e486641ad7a409a4a47
Farhan-Haseeb <syedfarhanhaseeb@gmail.com>
2019-12-10 16:09:56 +0500

    Setting DB in production

--- db/index.js
+++ db/index.js
@@ -5,17 +5,22 @@ const app = express();
 const env = app.get("env");
 let config = require("../config.json")[env];
 
-const sequelize = new Sequelize(config.database, config.username, config.password, {
-    host: config.host,
-    dialect: "postgres",
-    pool: {
-        max: 10,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-});
+let sequelize;
 
+if (config.store.use_env_variable) {
+    sequelize = new Sequelize(process.env[config.store.use_env_variable]);
+} else {
+    sequelize = new Sequelize(config.store.database, config.store.username, config.store.password, {
+        host: config.store.host,
+        dialect: config.store.dialect,
+        pool: {
+            max: 10,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    });
+}
 const Installations = InstallationsModel(sequelize, Sequelize);
 
 sequelize.sync({ force: true }).then(() => {
