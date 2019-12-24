305f808ff165a1869fd06c1a07b0b980349355c5
Hassan <hassan.karim@productbox.dev>
2019-12-24 15:07:49 +0500

    fix multiple project get issues

--- public/js/headlines.js
+++ public/js/headlines.js
@@ -1,8 +1,16 @@
 AJS.$(window).load(function () {
-    // let jwt_token = AJS.$('meta[name="token"]').attr("content");
     let jiraAccessToken = AJS.$('meta[name="jiraAccessToken"]').attr("content");
-    setInterval(function() {
-        const getSelectedProjectLinkUrl = `headlines?projectKey=${clickedProject}&repoNames=${clickedRepo}&jwt=${jiraAccessToken}`;
+    let clickedProject = '';
+    let projectKeys = [];
+    let clickedRepo = '';
+    let projectRepos = [];
+    setInterval(function () {
+        let getSelectedProjectLinkUrl = ''
+        if (clickedProject === 'All' || projectRepos === 'All') {
+            getSelectedProjectLinkUrl = `headlines?jwt=${jiraAccessToken}`
+        } else {
+            getSelectedProjectLinkUrl = `headlines?projectKey=${projectKeys}&repoNames=${projectRepos}&jwt=${jiraAccessToken}`;
+        }
         $.ajax({
             type: 'GET',
             url: getSelectedProjectLinkUrl,
@@ -13,18 +21,27 @@ AJS.$(window).load(function () {
                 $('.branches-container .jira-loader').addClass('hide-loader')
             }
         });
-    },10000)
-    
-    let clickedProject = null;
-    let clickedRepo = null;
+    }, 20000)
     $('.project-link').on('click', function (e) {
         $('.issues-container .jira-loader').removeClass('hide-loader')
         clickedProject = $(this).attr('name');
         let getSelectedProjectLinkUrl = null;
         if (clickedProject === 'All') {
+            const allCheckedProjects = $('#jira-projects .project-link');
+            for (let index = 0; index < allCheckedProjects.length; index++) {
+                if ($(allCheckedProjects[index]).attr('name') !== 'All') {
+                    $(allCheckedProjects[index]).prop('checked', false)
+                }
+            }
             getSelectedProjectLinkUrl = `headlines?jwt=${jiraAccessToken}`
-        } else {
-            getSelectedProjectLinkUrl = `headlines?projectKey=${clickedProject}&repoNames=${clickedRepo}&jwt=${jiraAccessToken}`;
+        }
+        if (e.target.checked && clickedProject !== 'All') {
+            projectKeys.push(clickedProject);
+            getSelectedProjectLinkUrl = `headlines?projectKey=${projectKeys}&repoNames=${projectRepos}&jwt=${jiraAccessToken}`;
+        }
+        if (!e.target.checked && clickedProject !== 'All') {
+            const removeIndex = projectKeys.indexOf(clickedProject);
+            projectKeys.splice(removeIndex, 1);
         }
         $.ajax({
             type: 'GET',
@@ -40,11 +57,24 @@ AJS.$(window).load(function () {
         $('.branches-container .github-loader').removeClass('hide-loader')
         clickedRepo = $(this).attr('name');
         let getSelectedProjectLinkUrl = null;
-        if (clickedRepo === 'All') {
+        if (e.target.checked && clickedProject === 'All') {
+            const allCheckedProjects = $('#github-repos .repositories-link');
+            for (let index = 0; index < allCheckedProjects.length; index++) {
+                if ($(allCheckedProjects[index]).attr('name') !== 'All') {
+                    $(allCheckedProjects[index]).prop('checked', false)
+                }
+            }
             getSelectedProjectLinkUrl = `headlines?jwt=${jiraAccessToken}`
-        } else {
-            getSelectedProjectLinkUrl = `headlines?projectKey=${clickedProject}&repoNames=${clickedRepo}&jwt=${jiraAccessToken}`;
         }
+        if (e.target.checked) {
+            projectRepos.push(clickedRepo);
+            getSelectedProjectLinkUrl = `headlines?projectKey=${projectKeys}&repoNames=${projectRepos}&jwt=${jiraAccessToken}`;
+        }
+        if (!e.target.checked) {
+            const removeIndex = projectRepos.indexOf(clickedProject);
+            projectRepos.splice(removeIndex, 1);
+        }
+
         $.ajax({
             type: 'GET',
             url: getSelectedProjectLinkUrl,
